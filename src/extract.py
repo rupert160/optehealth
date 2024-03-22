e03b383efd8dfa567b7e7dbb466dd40bbd46dd81
rupert160 <rupert@d2i.net.au>
2024-03-22 06:45:59 +0000

    download, process dict, get next page

--- src/extract.py
+++ src/extract.py
@@ -1,20 +1,88 @@
 #!/usr/bin/env python3
 
-import requests, os, pathlib
+import requests, os, json
+from bs4 import BeautifulSoup
 
-test_file = './test.html'
-test_uri = 'https://www.fao.org/3/AC854T/AC854T03.htm'
+test_domain = 'https://www.fao.org/3/AC854T/'
+target_file = 'AC854T03.htm'
+test_uri = test_domain + target_file
+output_dir = 'data/site/'
+output_file = output_dir + target_file
 
-def pull_test_file(uri:str, output_file:str):
-    r = requests.get(uri,output_file)
+def pull_test_file(uri:str, filepath:str):
+    r = requests.get(uri,filepath)
     r.status_code
-    with open(test_file,'w') as tw:
+    with open(filepath,'w') as tw:
         tw.write(r.text)
 
-def main():
-    if not os.path.isfile(test_file):
-        pull_test_file(test_uri, test_file)
+def str_to_int(input_str:str):
+    try:
+        return int(input_str)
+    except ValueError:
+        if input_str == '-':
+            return None
 
+def transform_html_to_dict(filepath):
+    html = ""
+    with open(filepath) as tw:
+        html = tw.read()
+    food = dict()
+    soup = BeautifulSoup(html,'html.parser')
+    row_index = 0
+    item_number = 0
+    food_name = ""
+    look_for_cc = False
+    look_for_B = False
+    for tr in soup.body.table.findAll('tr'):
+        division_index = 0
+        tds = tr.findAll('td')
+        ref_cell_contents = tds[0].getText()
+        #print("\n\n-------\n{}".format(ref_cell_contents))
+        #if ref_cell_contents == 'Item No.':
+        #    print("-- {}".format(ref_cell_contents == 'Item No.'))
+        if ref_cell_contents.isdigit():
+            item_number = int(ref_cell_contents)
+            food_name = tds[1].getText()
+            look_for_cc = True
+            look_for_B = True
+            #print("--- {}".format(ref_cell_contents))
+        elif ref_cell_contents == '(CC)':
+            look_for_cc = False
+        
+        if look_for_cc == False and look_for_B ==True:
+            if tds[6].getText() == 'B':
+                look_for_B = False
+                food[food_name] = {
+                    "Isoleucine":str_to_int(tds[7].getText()),
+                    "Leucine":str_to_int(tds[8].getText()),
+                    "Lysine":str_to_int(tds[9].getText()),
+                    "Methionine":str_to_int(tds[10].getText()),
+                    "Cystine":str_to_int(tds[11].getText()),
+                }
+        #for td in tds:
+        #    print("{}...{} --- {}".format(row_index, division_index, td.getText().strip()))
+        #    division_index += 1
+        row_index += 1
+    return food
+
+def get_next_page_uri(filepath):
+    with open(filepath) as tw:
+        html = tw.read()
+    soup = BeautifulSoup(html,'html.parser')
+    for a in soup.body.p.findAll('a'):
+        if a.img['alt'] == "Next Page":
+            return a['href']
+
+
+def main():
+    if not os.path.isfile(output_file):
+        pull_test_file(test_uri, output_file)
+
+    food = transform_html_to_dict(output_file)
+    next_uri = get_next_page_uri(output_file)
+        
+    print(json.dumps(food,indent=4))
+    print(next_uri)
 
 if __name__ == '__main__':
     main()
