dada525828a6b42423c9e6314a5a14aab5fac445
rupert160 <rupert@d2i.net.au>
2024-11-24 12:07:14 +0000

    autocomplete and table but redirect errors

--- src/main.py
+++ src/main.py
@@ -4,6 +4,13 @@ from typing import Union
 from fastapi import FastAPI, Depends
 from contextlib import asynccontextmanager
 
+from fastapi.responses import HTMLResponse
+from starlette.requests import Request
+from fastapi.staticfiles import StaticFiles 
+from fastapi.templating import Jinja2Templates 
+
+from rapidfuzz import process
+
 CACHE_PATH = '/opt/app/mnt/data/temp.json'
 
 class SessionManager:
@@ -34,6 +41,20 @@ app = FastAPI(lifespan=lifespan)
 def get_session():
     return session_manager
 
+app.mount("/static", StaticFiles(directory="static"), name="static") 
+templates = Jinja2Templates(directory="templates") 
+
+@app.get("/", response_class=HTMLResponse) 
+async def init(request: Request, session: SessionManager = Depends(get_session)):
+    results = list(session.get("data").keys())
+    return templates.TemplateResponse("index.html", {"request": request, "results": results})
+
+@app.get("/search", response_class=HTMLResponse)
+async def search(request: Request, query: str, session: SessionManager = Depends(get_session)):
+    #results = process.extract(query, session.get("data").keys(),limit=3)
+    results2 = session.get("data")[query]
+    return templates.TemplateResponse("index.html", {"request": request, "results2": [query,results2]})
+
 @app.get("/items/")
 async def read_root(session: SessionManager = Depends(get_session)):
     return {"message": "Hello, World!", "session": session.get("data")}
@@ -43,8 +64,7 @@ async def read_root( item_id: str , q: Union[str, None] = None
                     , session: SessionManager = Depends(get_session)):
     return {"message": item_id, "q": session.get("data")[item_id]}
 
-
-@app.post("/items/")
-async def update_session(new_data: dict, session: SessionManager = Depends(get_session)):
-    session.set("data", new_data)
-    return {"message": "Session updated", "session": session.get("data")}
+#@app.post("/items/")
+#async def update_session(new_data: dict, session: SessionManager = Depends(get_session)):
+#    session.set("data", new_data)
+#    return {"message": "Session updated", "session": session.get("data")}
